8b2249207afdde72c1bf41c96e3eb6b1d96ad525
reichmannwosisdes234 <reichmannwosisdes234@gmail.com>
2025-03-24 08:26:31 +0000

    Add 18.py

---
+++ src/18.py
@@ -0,0 +1,16 @@
+import math
+
+def calculate_square_root(number):
+    """
+    Calculate and return the square root of a given number.
+    
+    Args:
+    number (float): The number to calculate the square root of.
+    
+    Returns:
+    float: The square root of the input number.
+    """
+    return math.sqrt(number)
+
+# Test the function
+print(calculate_square_root(9))
