f1840272e631e752a5b6adfc17f4c088dc698ed5
reichmannwosisdes234 <reichmannwosisdes234@gmail.com>
2025-03-29 19:03:46 +0000

    Add 19.py

---
+++ src/19.py
@@ -0,0 +1,25 @@
+import math
+a = 5
+b = 3
+c = 4
+
+d = a + b
+e = c ** 2
+f = d / e
+g = math.sqrt(16)
+h = -8.0
+i = math.sin(math.radians(72))
+j = math.log(10, 3)
+k = math.pow(5, 2)
+l = math.pi / 4
+m = (4 ** 0.5) + 1
+n = math.factorial(6)
+o = math.e ** 3
+p = -math.sqrt(-9)
+q = math.tan(math.radians(78))
+r = math.atan2(3, 4)
+s = math.cos(math.radians(20))
+t = (5 / 2) + 1.5
+u = pow(2, 0.5)
+v = math.e ** (-9)
+w = -math.sqrt(-9.0)
