e338029fcc1fe997f12e64af23eb86795c246b76
reichmannwosisdes234 <reichmannwosisdes234@gmail.com>
2025-03-30 18:38:48 +0000

    Add 20.py

---
+++ src/20.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def calculate_area_rectangle(width, height):
+    """
+    Calculate the area of a rectangle.
+    
+    Args:
+    width (float): The width of the rectangle.
+    height (float): The height of the rectangle.
+
+    Returns:
+    float: The area of the rectangle.
+    """
+    return width * height
