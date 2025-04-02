71ccfa06a7b6157c53d6901299d9cf7bebedb322
reichmannwosisdes234 <reichmannwosisdes234@gmail.com>
2025-04-02 14:11:09 +0000

    Add 23.py

---
+++ src/23.py
@@ -0,0 +1,6 @@
+def calculate_square_root(number):
+    return number ** 0.5
+
+number = float(input("Enter a number: "))
+result = calculate_square_root(number)
+print(f"The square root of {number} is {result}")
